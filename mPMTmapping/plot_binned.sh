7fdf5642fa2c91873d131333d3d61a1e95ba583f
Alie Craplet <alie.craplet@gmail.com>
2022-12-21 16:50:12 +0000

    code to plot a single file both binned and unbinned

---
+++ mPMTmapping/plot_binned.sh
@@ -0,0 +1,17 @@
+#this is a called code to plot and save the plot of the effeciency map and comparision between different files
+
+ID_1=$1
+mode=binned
+
+if [[ $# -eq 2 ]]
+then
+    ID_2=$2
+    echo "Plotting comparision of the two files "$ID_1" and "$ID_2""
+    python src/read_txtMap_binned.py -f maps_txtFiles/mPMT_map_ID$ID_1.txt -c maps_txtFiles/mPMT_map_ID$ID_2.txt -o "comparision_FileID"$ID_1"_FileID"$ID_2""$mode""
+fi
+
+if [[ $# -eq 1 ]]
+then
+    echo "Plotting the map of the file "$ID_1""
+    python src/read_txtMap_binned.py -f Maps/maps_txtFiles/mPMT_map_ID$ID_1.txt -o "plotting_FileID"$ID_1""$mode""
+fi
