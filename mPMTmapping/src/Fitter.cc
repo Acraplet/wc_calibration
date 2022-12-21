c0481f99b463d7c4d114041b4ae1c7936526b562
Alie Craplet <alie.craplet@gmail.com>
2022-12-21 15:17:43 +0000

    added and cleaned the code to make the oneposition reference which is then used by the Fitter to extract the behaviour of each position as the attenaution and scattering lengths are increased

--- mPMTmapping/src/Fitter.cc
+++ mPMTmapping/src/Fitter.cc
@@ -11,22 +11,18 @@
 #include "TH1.h"
 #include "TGraphErrors.h"
 
-
-
 int main(int argc, char **argv){
     //we need the true value of alpha
-    std::cout << "hello world" << std::endl;
-    std::cout << truth_alpha(401.9, 0.000486, 10e10) << std::endl;
-
     std::fstream newfile;
-    newfile.open(Form("/home/ac4317/Laptops/Year1/WCTE/wc_calibration/mPMTmapping/maps_txtFiles/mPMT_map_ID%s.txt",argv[1]),std::ios::in); //open a file to perform read operation using file object
+    //This is the configuration of which we are going to fit all of the source positions to get the reference
+    //from the OnePositon files
+    newfile.open(Form("/home/ac4317/Laptops/Year1/WCTE/wc_calibration/mPMTmapping/Maps/maps_txtFiles/mPMT_map_ID%s.txt",argv[1]),std::ios::in); //open a file to perform read operation using file object
     std::vector<double> list_R, list_A;
     char* R_test;
     if (newfile.is_open()){   //checking whether the file is open
         std::string tp;
         while(getline(newfile, tp)){  //read data from file object and put it into string.
             //this is for each test source position
-            //             std::cout << tp << "  " << std::endl;   //print the data of the string
             char *ptr;
             //convert to s char the string of the line we are extracting
             char* character = std::strcpy(new char[tp.length() + 1], tp.c_str());
@@ -36,8 +32,6 @@ int main(int argc, char **argv){
             char* theta_test;
             double  theta_test_num;
             char* phi_test;
-
-
             while (ptr != NULL)
                 //loop over the characteristics of the given position
             {
@@ -59,16 +53,17 @@ int main(int argc, char **argv){
                 i +=1;
             }
             //Now we have the coordinate of the position we are looking for
-            //Next: open the reference txt file for this position and from there extract the fitting information we are looking for
+            //Next: open the reference txt file for this position and from 
+	    //there extract the fitting information we are looking for
             std::fstream position_file;
-            position_file.open(Form("/home/ac4317/Laptops/Year1/WCTE/wc_calibration/mPMTmapping/maps_onePosition/OnePosition_theta%s_phi%s_R%s.txt", theta_test, phi_test, R_test),std::ios::in);
+            position_file.open(Form("/home/ac4317/Laptops/Year1/WCTE/wc_calibration/mPMTmapping/Maps/maps_onePosition/OnePosition_theta%s_phi%s_R%s.txt", theta_test, phi_test, R_test),std::ios::in);
 
             if (position_file.is_open()){   //checking whether the file is open
                 std::string tp_ref;
-                std::vector<double> data_xval, err_xval, data_scat_xval, err_scat_xval; //the variable you're fitting in
+		//the variable you're fitting in
+                std::vector<double> data_xval, err_xval, data_scat_xval, err_scat_xval; 
                 std::vector<double> data_yval, err_yval, data_scat_yval, err_scat_yval;
                 while(getline(position_file, tp_ref)){ //each reference point
-                    //                     std::cout << tp_ref << "  " << std::endl;
                     char *ptr_ref;
                     //convert to s char the string of the line we are extracting
                     char* character_ref = std::strcpy(new char[tp_ref.length() + 1], tp_ref.c_str());
@@ -79,57 +74,42 @@ int main(int argc, char **argv){
                     while (ptr_ref != NULL)
                     {//loop over the characteristics of the given position
                         if (j==6){
-                            //std::cout << "Q : " << ptr_ref  << std::endl; // print the string token
                             std::string fs(ptr_ref);
                             Q_ref=std::stof(fs);
-                            //std::cout << Q_ref << std::endl;
                         }
                         if (j==8){
-                            //std::cout << "abwff : " << ptr_ref  << std::endl; // print the string token
                             std::string fs(ptr_ref);
                             abwff=std::stof(fs);
-                            //                             float  = (float) (ptr_ref)
-                            //std::cout << truth_alpha(401.9, abwff, 10e10);
                         }
                         if (j==9){
-                            //std::cout << "rayff : " << ptr_ref  << std::endl; // print the string token
                             std::string fs(ptr_ref);
                             rayff=std::stof(fs);
-                            //std::cout << truth_alpha(401.9, 10e10, rayff) << std::endl;
                         }
                         ptr_ref = std::strtok (NULL, " ");
                         j +=1;
                     }//characteristic of the given position
-                    //                     std::cout << abwff << " and "<< rayff<< std::endl;
                     //now append to the correct data points:
                     if (abwff <= 1e10 && rayff >= 1e10){
                         double alpha_abs = truth_alpha(401.9, abwff, rayff);
                         data_xval.push_back(alpha_abs);
-                        //std::cout << "Value :" << alpha_abs << " " << abwff << " "<< Q_ref << std::endl;
                         data_yval.push_back(Q_ref);
                         err_xval.push_back(0);
                         err_yval.push_back(TMath::Sqrt(Q_ref * (1 - Q_ref/1000)));
-                        //                         std::cout << std::endl;
                     }
-                    //                  This is the scattering part
+                    //Scattering data points
                     if (abwff >= 1e10 && rayff <= 1e10){
                         double alpha_scat = truth_alpha(401.9, abwff, rayff);
                         data_scat_xval.push_back(alpha_scat);
-                        //std::cout << "Value :" << alpha_abs << " " << abwff << " "<< Q_ref << std::endl;
                         data_scat_yval.push_back(Q_ref);
                         err_scat_xval.push_back(0);
                         err_scat_yval.push_back(TMath::Sqrt(Q_ref * (1 - Q_ref/1000)));
-                        //                         std::cout << std::endl;
                     }
                 }//each reference point - now set up the fitting
-                int nPars = 2;
-
-                //                 std::cout << data_yval[4] << std::endl;
-                //              This is the absorption part
+                int nPars = 2; //Amplitude and R the distance from the mPMT
+                //This is the absorption part
                 Chisq *chi = new Chisq(nPars);
                 chi->setData(data_xval, data_yval);
                 ROOT::Math::Functor f(chi, &Chisq::fcn, nPars);
-
                 ROOT::Math::Minimizer *min = ROOT::Math::Factory::CreateMinimizer("Minuit2", "Migrad");
                 min->SetStrategy(3);
                 min->SetFunction(f);
@@ -139,17 +119,14 @@ int main(int argc, char **argv){
                 min->SetVariable(1, "radius", 1.0, 0.01);
 
                 min->Minimize();
-                //                 min->PrintResults();
-                //                 std::cout << std::endl;
                 const double * res = min->X();
                 const double * err = min->Errors();
-                //                 std::cout << min->Status() << std::endl;
                 if (min->Status() == 0) {
                     list_A.push_back(res[0]);
                     list_R.push_back(res[1]);
                 }
                 std::cout << res[0] << std::endl;
-
+		//put the information in root objects
                 TF1 func = chi->getFunction(0, 220, "best_fit_abs");
                 TGraphErrors *data = new TGraphErrors(data_xval.size(), &data_xval[0], &data_yval[0], &err_xval[0], &err_yval[0]);
                 TGraphErrors *fit_output;
@@ -164,71 +141,31 @@ int main(int argc, char **argv){
 
 
                 //This is the scattering part
-                nPars = 5;
+                nPars = 5; // the y of the 5  nodes for the fake-data spline
                 Chisq *chi_scat = new Chisq(nPars);
                 chi_scat->setData(data_scat_xval, data_scat_yval);
-
                 ROOT::Math::Functor f_scat(chi_scat, &Chisq::fcn_rayff, nPars);
-
                 ROOT::Math::Minimizer *min_scat = ROOT::Math::Factory::CreateMinimizer("Minuit2", "Migrad");
                 min_scat->SetStrategy(3);
                 min_scat->SetFunction(f_scat);
                 min_scat->SetMaxFunctionCalls(10000);
-
-                min_scat->SetVariable(0, "a", 0.0, 0.01);
+                //At the moment we are working with 5 nodes the y of these 5 nodes is what we fit for the scattering
+		min_scat->SetVariable(0, "a", 0.0, 0.01);
                 min_scat->SetVariable(1, "b", 1.0, 0.01);
                 min_scat->SetVariable(2, "c", 0.0, 0.01);
                 min_scat->SetVariable(3, "d", 1.0, 0.01);
                 min_scat->SetVariable(4, "e", 1.0, 0.01);
-                //                 min_scat->SetVariable(5, "f", 0.0, 0.01);
-                //                 min_scat->SetVariable(6, "g", 1.0, 0.01);
-                //                 min_scat->SetVariable(7, "h", 0.0, 0.01);
-                //                 min_scat->SetVariable(8, "i", 1.0, 0.01);
-                //                 min_scat->SetVariable(9, "j", 1.0, 0.01);
-
-                min_scat->Minimize();
                 min_scat->PrintResults();
-
-                //Save as an histogram the result? yes cause it is a single position?
+                //Save as a TGraphError the charge collected at this given position as a function 
+		//of the attenuation and then scattering length
                 const double * res_scat = min_scat->X();
                 const double * err_scat = min_scat->Errors();
                 double x_pos[5] = {5., 25., 75., 150., 220.};
                 double x_err[5] = {0., 0., 0., 0., 0.};
-
-                //                 for (int r = 0; r<=res.size(); r++){
-                //                     node_x.push_back();
-                //
                 TGraphErrors *gr = new TGraphErrors(5,x_pos,res_scat,x_err,err_scat);
                 gr->SetTitle("Scattering Spline nodes");
-
-
-                //                 const double * res = min->X();
-                //                 const double * err = min->Errors();
-
-                //                 if (min->Status() == 0) {
-                //                     list_A.push_back(res[0]);
-                //                     list_R.push_back(res[1]);
-                //                 }
-
-                TF1 func_scat = chi_scat->getFunction_rayff(0, 240, "best_fit_scat");
-
                 TGraphErrors *data_scat = new TGraphErrors(data_scat_xval.size(), &data_scat_xval[0], &data_scat_yval[0],&err_scat_xval[0], &err_scat_yval[0]);
-                //                 TGraphErrors *fit_output_scat;
-                // //                 if (min_scat->Status() == 0){
-                //                     fit_output_scat = new TGraphErrors(1, &res_scat[0], &res_scat[1], &err_scat[0], &err_scat[1]);
-                // //                 }
-                //                 else {
-                //                     auto a_scat = res_scat[0] * 0;
-                //                     fit_output_scat = new TGraphErrors(1, &a_scat, &a_scat, &a_scat, &a_scat);
-                //                 }
-
-                //              This will be the scattering part
-                //              Need to fit a spline to the data we have and save the data and the fit
-
-                //                 TSpline *scatSpline = new TSpline("scatterning spline", 10., 5., 220., 10, false);
-                //                 scatSpline->Draw();
-
-                //              This is saving everything
+                //This is saving everything the fit output in the reference_root folder
                 TFile *outf = new TFile(Form("reference_root/results_Abs_Scat_theta%s_phi%s_R%s.root", theta_test, phi_test, R_test), "RECREATE");
 
                 func.SetTitle("absorption_best_fit");
@@ -251,21 +188,21 @@ int main(int argc, char **argv){
                 fit_output->GetYaxis()->SetTitle("Distance R to the mPMT dome (cm)");
                 fit_output->Write("fit_output_xA_yR");
                 outf->Close();
-            }//fiunished reading the reference file for the given position
+            }//finished reading the reference file for the given position
         }
         newfile.close();   //close the file object.
-        //now plot the histogram -
+        //now plot the histogram - to check whether the fitted attenuation is consistent with
+	//an exponential behaviour
         TH1* h_R = new TH1D("h_R", Form("Histogram of Fitted R - R True: %s", R_test), 100,0.,100.);
         TH1* h_A = new TH1D("h_A", Form("Histogram of Fitted A - R True: %s", R_test), 100,200.,400.);
         for (long unsigned int u=0; u<list_R.size(); u++){
-            //         std::cout <<  list_R[u] << std::endl;
             h_R->Fill(list_R[u]);
             h_A->Fill(list_A[u]);
 
         }
         h_R->GetXaxis()->SetTitle("Fitted R(cm)");
         h_A->GetXaxis()->SetTitle("Fitted max charge per 1000 photon");
-        TFile f(Form("histos_R%s.root", R_test), "RECREATE");
+        TFile f(Form("histogram_fit/histos_R%s.root", R_test), "RECREATE");
         h_A->Write();
         h_R->Write();
         f.Close();
---
+++ mPMTmapping/src/make_onePositionMap.cc
@@ -0,0 +1,159 @@
+//This stores the soruce position xyz, Rtp and the charge and total number of photons 
+//Any map that is called by this code should be a reference map as the information will be appended to the end 
+//of the reference "onePosition" files. These are intended as a profile for how he charge collected evolves with 
+//absorption and scattering length and from there be a look-up table for extracting the attenuation length 
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <TROOT.h>
+#include <TApplication.h>
+#include <TStyle.h>
+#include <TFile.h>
+#include <TChain.h>
+#include <TTree.h>
+#include <TCanvas.h>
+#include <TChain.h>
+#include <TBranch.h>
+#include <TH1.h>
+#include <TH2.h>
+#include <TH3.h>
+#include <TMath.h>
+#include <TGraph.h>
+#include <TVector3.h>
+#include <TRandom3.h>
+
+#include "ColorOutput.hh"
+const std::string ERR = color::RED_STR + "[ERROR]: " + color::RESET_STR;
+const std::string WAR = color::GREEN_STR + "[WARNING]: " + color::RESET_STR;
+
+void HelpMessage()
+{
+    std::cout << "USAGE: "
+    << "make_mPMTmap" << "\nOPTIONS:\n"
+    << "-f : Input file\n" << std::endl
+    << "-o : Output file\n" << std::endl;
+}
+
+int main(int argc, char **argv){
+    bool verbose=false;
+    char * filename=NULL;
+    char * outfilename=NULL;
+    long int endEvent=0;
+    long int startEvent=0;
+    char c;
+
+    while( (c = getopt(argc,argv,"f:o:b:s:e:l:r:p:c:w:hdtv")) != -1 ){//input in c the argument (-f etc...) and in optarg the next argument. When the above test becomes -1, it means it fails to find a new argument.
+        switch(c){
+            case 'f':
+                filename = optarg;
+                break;
+        }
+        switch(c){
+            case 'o':
+                outfilename = optarg;
+                break;
+        }
+    }
+
+    if (filename==NULL){
+        std::cout << ERR << "Error, no input file: " << std::endl;
+        HelpMessage();
+        return -1;
+    }
+
+    if (outfilename==NULL){
+        std::cout << WAR << "Warning, no output file given" << std::endl;
+    }
+
+    std::string x, y, z, t, p, R; //the cartesian and polar coordinate, careful R is from the surface of the mPMT dome and not from its centre of the mPMT
+    int str; //this will help break down the string
+    //To read the characteristics of the file
+    std::string T;
+    std::string abwff;
+    std::string rayff;
+    std::stringstream X(filename);
+    while(std::getline(X, T, '_')){
+        if (!T.find("Absff")){
+            str = T.find("Absff");
+            abwff = T.erase(0,str+5);
+        }
+        if (!T.find("Rayff")){
+            str = T.find("Rayff");
+            rayff = T.erase(0,str+5);
+            // Deletes str+5 characters from index number 0
+        }
+        if (!T.find("x")){
+            str = T.find("x");
+            x = T.erase(0,str+1);
+        }
+        if (!T.find("y")){
+            str = T.find("y");
+            y = T.erase(0,str+1);
+        }
+        if (!T.find("z")){
+            str = T.find("z");
+            z = T.erase(0,str+1);
+        }
+        if (!T.find("t")){
+            str = T.find("t");
+            t = T.erase(0,str+1);
+        }
+        if (!T.find("p")){
+            str = T.find("p");
+            p = T.erase(0,str+1);
+        }
+        if (!T.find("R")){
+            str = T.find("R");
+            R = T.erase(0,str+1);
+        }
+    }
+    std::cout << "Source position : " << x << " " << y << " " << z << " " << t << " " << p << " " << R << std::endl;
+    std::cout << "Absorption : " << abwff << " Scattering : " << rayff << std::endl;
+
+    TFile *infile = new TFile(filename, "READ");
+    TTree *events = (TTree*)infile->Get("CherenkovHits");
+
+    int event;
+    int NHits;
+    int mPMT;
+    int mPMT_pmt;
+    int nHitsTot = 0;
+
+    events->SetBranchAddress("Event", &event);
+    events->SetBranchAddress("NHits", &NHits);
+    events->SetBranchAddress("mPMT", &mPMT);
+    events->SetBranchAddress("mPMT_pmt", &mPMT_pmt);
+
+    int n_entries = events->GetEntries();
+    for(int i=0; i<n_entries; i++){
+        events->GetEntry(i);
+	if (mPMT == 58){
+		if (mPMT_pmt != 19){
+        		nHitsTot += NHits;
+		}
+	}
+	if (mPMT == 59){
+                if (mPMT_pmt == 19){
+                        nHitsTot += NHits;
+                }
+        }
+    }
+    std::cout << "Total number of hits : "<< nHitsTot << std::endl;
+    std::ofstream outfile;
+    outfile.open(outfilename, std::ofstream::app);
+    outfile <<  x << " " << y << " " << z << " " << t << " " << p << " " << R << " " << nHitsTot << " " << n_entries << " " << abwff << " " << rayff << std::endl;
+
+    const char* theta = t.c_str();
+    const char* phi = p.c_str();
+    const char* dist = R.c_str();
+
+    std::string onePosition_outfile = Form("/home/ac4317/Laptops/Year1/WCTE/wc_calibration/mPMTmapping/Maps/maps_onePosition/OnePosition_theta%s_phi%s_R%s.txt", theta, phi, dist);
+    std::cout << onePosition_outfile << std::endl;
+    //carefull, we are appending to the end of the existing file, 
+    //if this file is not a reference file it will cause problems
+    std::ofstream onePosition;
+    onePosition.open(onePosition_outfile, std::ofstream::app);
+    onePosition <<  x << " " << y << " " << z << " " << t << " " << p << " " << R << " " << nHitsTot << " " << n_entries << " " << abwff << " " << rayff << std::endl;
+}//End of main
