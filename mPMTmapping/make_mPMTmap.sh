dd6aed3593e85de0257949470fa8ee7171cba95e
--replace-all <alie.craplet@gmail.com>
2023-01-30 09:54:15 +0000

    working on making the code runnable from everywhere

--- mPMTmapping/make_mPMTmap.sh
+++ mPMTmapping/make_mPMTmap.sh
@@ -2,7 +2,8 @@
 #called with bash make_mPMTmap.sh ID where ID is the FileID that we want and the flat files are stored in the
 #/home/ac4317/Laptops/Year1/WCTE/wc_calibration/mPMTmapping/data/ folder
 ID=$1
-VAR="$(ls /home/ac4317/Laptops/Year1/WCTE/wc_calibration/mPMTmapping/data/*FileID""$ID""_*_flat*)"
+#VAR="$(ls /home/ac4317/Laptops/Year1/WCTE/wc_calibration/mPMTmapping/data/*FileID""$ID""_*_flat*)"
+VAR="$(/vols/t2k/users/ac4317/WCTE/WCSim/mPMTmapping/data/*FileID""$ID""_*_flat*)"
 make
 
 rm Maps/maps_txtFiles/mPMT_map_ID"$ID".txt
